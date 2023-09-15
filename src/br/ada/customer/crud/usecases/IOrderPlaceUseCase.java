1875cf307d0e4bac11326f0b75b11889863acc6c
Dayane <acdayane@gmail.com>
2023-09-15 17:39:59 -0300

    feat: add cart item

--- src/br/ada/customer/crud/usecases/IOrderPlaceUseCase.java
+++ src/br/ada/customer/crud/usecases/IOrderPlaceUseCase.java
@@ -13,7 +13,6 @@ public interface IOrderPlaceUseCase {
      */
 
     
-
     void placeOrder(Order order);
 
 }
---
+++ src/br/ada/customer/crud/usecases/impl/OrderItemUseCaseImpl.java
@@ -0,0 +1,81 @@
+package br.ada.customer.crud.usecases.impl;
+
+import java.math.BigDecimal;
+
+import br.ada.customer.crud.model.Order;
+import br.ada.customer.crud.model.OrderItem;
+import br.ada.customer.crud.model.OrderStatus;
+import br.ada.customer.crud.model.Product;
+import br.ada.customer.crud.usecases.INotifierUserCase;
+import br.ada.customer.crud.usecases.IOrderItemUseCase;
+import br.ada.customer.crud.usecases.repository.OrderRepository;
+
+public class OrderItemUseCaseImpl implements IOrderItemUseCase {
+
+    private OrderRepository orderRepository;
+    private INotifierUserCase<Order> orderNotifier;
+
+    public OrderItemUseCaseImpl(OrderRepository repository, INotifierUserCase<Order> notifier) {
+        this.orderRepository = repository;
+        this.orderNotifier = notifier;
+    }
+     /*
+     * 1 - Pedido precisa estar com status == OrderStatus.OPEN
+     * 2 - Lembrar de atualizar o banco através do repository
+     */
+    @Override
+    public OrderItem addItem(Order order, Product product, BigDecimal price, Integer amount) {
+
+        OrderItem newOrderItem = new OrderItem();
+                
+        if (order.getStatus() != OrderStatus.OPEN) {
+            throw new RuntimeException("Pedido não está aberto");
+        }
+
+        newOrderItem.setProduct(product);
+        newOrderItem.setSaleValue(price);
+        newOrderItem.setAmount(amount);
+
+        order.getItems().add(newOrderItem);        
+
+        orderRepository.save(order);
+        orderNotifier.registered(order);
+
+        return newOrderItem;
+        
+    }
+
+     /*
+     * 1 - Pedido precisa estar com status == OrderStatus.OPEN
+     * 2 - Trocar a quantidade que foi vendida desse produto
+     * 3 - Lembrar de atualizar o banco através do repository
+     */
+    @Override
+    public OrderItem changeAmount(Order order, Product product, Integer amount) {
+
+        OrderItem changeAmouItem = new OrderItem();
+      
+        if (order.getStatus() != OrderStatus.OPEN) {
+            throw new RuntimeException("Pedido não está aberto");
+        }
+
+        for (OrderItem item : order.getItems()) {
+            if (item.equals(product)) {
+                
+            }
+        }
+
+        return changeAmouItem;
+    }
+
+    
+    /*
+     * 1 - Pedido precisa estar com status == OrderStatus.OPEN
+     * 2 - Lembrar de atualizar o banco através do repository
+     */
+    @Override
+    public void removeItem(Order order, Product product) {
+       
+    }
+    
+}
